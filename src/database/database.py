42593eb7ef1fdf9f142298b291ec7e79fe9c8593
KadeApricot <k4deapricot.std@gmail.com>
2026-07-02 19:50:12 +0700

    SQLite3 database implementation

--- src/database/database.py
+++ src/database/database.py
@@ -1,18 +1,45 @@
+from abc import ABC, abstractmethod
+
 from database.record import Record
 
 
-class Database:
+class Database(ABC):
+    @abstractmethod
     def connect(self):
         pass
-
-    def execute(self, query, params):
+    
+    @abstractmethod
+    def ensure_connection(self):
+        pass
+    
+    @abstractmethod
+    def disconnect(self):
+        pass
+    
+    @abstractmethod
+    def initialize(self):
+        pass
+    
+    @abstractmethod
+    def reset(self):
+        pass
+    
+    @abstractmethod
+    def hard_reset(self):
+        pass
+    
+    @abstractmethod
+    def execute(self, query, params = None):
         pass
 
+    @abstractmethod
     def save(self, obj: Record):
         pass
 
+    @abstractmethod
     def load(self, obj: Record):
         pass
 
+    @abstractmethod
     def delete(self, obj: Record):
         pass
--- src/database/record.py
+++ src/database/record.py
@@ -1,3 +1,9 @@
+from serialization.obj_codec import ObjTypeRegistry
+
 class Record:
-    def __init__(self):
-        self.id = None
+    def __init__(self, name: str = None):
+        self.name = name
+        
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        ObjTypeRegistry.register(cls)
